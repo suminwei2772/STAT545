25dd9065f122e758e569e91e35501e23cae0cb8d
Lisa Wei <lisawei18@gmail.com>
2017-11-12 21:21:32 -0800

    commit2

--- hw07/step3_draw_fig.R
+++ hw07/step3_draw_fig.R
@@ -7,5 +7,9 @@ args <- commandArgs(TRUE)
 
 knitr::opts_chunk$set(echo=TRUE)
 en <- read_tsv(args[1], col_names="Length")
-fig <- ggplot(en) + aes(x=Length) + geom_histogram(binwidth=1) 
+fig <- ggplot(en) + 
+					aes(x=Length) + 
+					geom_histogram(binwidth=1) +
+					ggtitle("Distribution of Word Lengths")
+
 ggsave("length_distribution.png", fig)
